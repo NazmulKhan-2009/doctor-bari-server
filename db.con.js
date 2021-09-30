0b498b28200329950e083688ae8472f2032db2e5
NazmulKhan-2009 <nazmulustc09@gmail.com>
2021-09-30 22:37:03 +0600

    update routes

---
+++ db.con.js
@@ -0,0 +1,27 @@
+
+require('dotenv').config()
+const mongoose=require('mongoose')
+
+//!from doctor bari api
+const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnbwm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+//! from hospital api
+
+
+// const localDB="mongodb://localhost:27017/hospital?retryWrites=true&w=majority"
+
+module.exports = async function connection() {
+ try {
+     const connectionParams = {
+         useNewUrlParser: true,
+         
+         useUnifiedTopology: true,
+     };
+     await mongoose.connect(db, connectionParams);
+     console.log("connected to database");
+ } catch (error) {
+     console.log(error);
+     console.log("could not connect to database");
+ }
+};
--- index.js
+++ index.js
@@ -1,167 +1,25 @@
-console.clear()
+require('dotenv').config();
 
-
-const express=require('express')
-const bodyParser=require('body-parser');
-const cors=require('cors');
-const fileUpload=require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connection=require('./db.con');
+const app=express();
 const port=process.env.PORT || 8000;
 
-const app = express();
+const hospitalRoute=require('./src/router/hospitalData');
+const doctorRoute=require('./src/router/doctorRouter')
 
-// Middleware Config
+connection()
+
+
+app.use(express.json({limit: '600kb'}));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static('doctors')); //directory send for photo upload
-app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/hospital',hospitalRoute);
+app.use('/doctor',doctorRoute);
 
-//mongo configure
-
-
-
-// const uri = `mongodb+srv://docNazmul:docNazmulkhan@cluster0.qnbwm.mongodb.net/doctorBari?retryWrites=true&w=majority`;
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnbwm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-console.log(client ? 'Data Base Connected' : "Sorry DB Not connected")
-
-client.connect(err => {
-  const appointmentCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_APPOINTMENT_COLLECTION}`);
-
-  const doctorCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_DOCTOR_COLLECTION}`);
-  // const doctorCollection = client.db(`doctorBari`).collection(`docInfo`);
-
- app.post('/addAppointment' , (req , res)=>{
-   const appointment=req.body
-   console.log(appointment)
-
-   appointmentCollection.insertOne(appointment)
-   .then(response =>res.send(response.insertedCount>0)) 
-  
- })
-
- app.get("/appointments",(req,res)=>{
-  appointmentCollection.find({}).toArray((err,result)=>{res.send(result)})
- }) 
-
- //PATCH SEEN
-//  app.put("/addAppointment/:id", (req,res)=>{
-//   appointmentCollection.findByIdAndUpdate({_id:req.params.id},req.body)
-//   .then(res=>console.log(res))
-  
-  
-//  })
-
- app.post('/addadoctor',(req,res)=>{
-   const file=req.files.file;
-   const name=req.body.name;
-   const email=req.body.email;
-  //  const filePath=`${__dirname}/doctors/${file.name}`
-
-  //  console.log(file, name , email)
-  //  console.log(file.data)
-
-  console.log(typeof(file.size))
-  console.log(typeof(parseInt(file.size)))
-  //  file.mv(filePath,err=>{
-  //    if(err){
-  //     console.log(err)
-  //     return res.status(500).send({msg:"Falied to upload"})
-  //    }
-//not part of code just keep for later
-  // const newImg=fs.readFileSync(filePath)
-  const newImg=file.data
-  const encImg=newImg.toString('base64')
-
-  // Buffer depricated so use Buffer.alloc(), Buffer.allocUnsafe(), or Buffer.from() 
-
-  var image={
-    contentType:file.mimetype,
-    size:file.size,
-    // size:parseInt(file.size),
-    // img:Buffer(encImg,'base64')
-    img: Buffer.from(encImg,'base64')
-  }
-
-     doctorCollection.insertOne({img:image,name:name,email:email})
-     .then(result=>{
-       res.send(result.insertedCount>0);
-     })
-   })
-
-//  })
-
- app.get('/doctorlist',(req,res)=>{
-  doctorCollection.find({}).toArray((err,result)=>{res.send(result)})
- })
-
-// appointment by date
-// app.post('/appointmentbydate', (req, res) => {
-//   const date = req.body.date;
-//   const email = req.body.email;
-//   doctorCollection.find({ email: email })
-//       .toArray((err, doctors) => {
-//           const filter = { appointmentDate:date }
-//           if (doctors.length === 0) {
-//               filter.email = email;
-//           }
-//           appointmentCollection.find(filter)
-//               .toArray((err, documents) => {
-//                   // console.log(email, date, doctors, documents)
-//                   console.log(documents)
-//                   res.send(documents);
-//               })
-//       })
-// })
-
-
-app.post('/appointmentbydate',async(req,res)=>{
-const info={
-  email:req.body.email,
-  appointmentDate:req.body.date
-}
-
-try{
-  await doctorCollection.find({email:req.body.email}).toArray((err,doctors)=>{
-    // if(doctors.length>0){
-    //   appointmentCollection.find({appointmentDate:req.body.date}).toArray((err,docs)=>{console.log(docs)})
-    // }else{
-    //   appointmentCollection.find(info).toArray((err,appoint)=>{console.log(appoint)})
-    //   // console.log("no doctor in the list")
-    // }
-
-    (async()=>{
-      doctors.length>0 ? 
-      await appointmentCollection.find({appointmentDate:req.body.date}).toArray((err,docs)=>{res.send(docs)})      
-      :
-      await appointmentCollection.find(info).toArray((err,appoint)=>{res.send(appoint)})
-
-    })()
-
-  })
-
-
- }catch(e){
-   res.status().send(e)
-  } ;
-
-
-})
-
-app.post('/isdoctor' , async(req , res)=>{
-  await doctorCollection.find({email:req.body.email}).toArray((err,docotorList)=>{
-    console.log(docotorList.length>0)
-    res.send(docotorList.length>0)
-  })
-})
-
-
-});
 
 
 
